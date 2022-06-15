03d0eea1bee61126865502dc5738b3d1dd9b922d
vishal <vishalguptaramasare@gmail.com>
2022-06-15 10:43:03 +0530

    changes by vishal

--- clientStoriesDetailPage.php
+++ clientStoriesDetailPage.php
@@ -15,101 +15,116 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 p-4">
-            <i class="fa-solid fa-arrow-left-long"></i>
+                <i class="fa-solid fa-arrow-left-long"></i>
             </div>
         </div>
     </div>
     <div class="container-fluid">
+
+        <div class="row justify-content-center">
+            <div class="col-11">
+                <div class="row">
+                    <div class="col-md-7">
+                        <!-- <div class="col-md-8"> -->
+                        <div>
+                            <h5 class="primaryHeading mb-4">Rendering extensive start-up services to our clients!</h5>
+                            <p class="normalText text-justify mb-4">KEO Films, which had recently won a contract to make an Australian adaptation of a UK TV
+                                series, was put in touch with Ecovis Sydney in November 2012 because they needed to
+                                establish their imprint quickly, before Christmas!</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="col-md-3 primaryHeading">The Context</div>
+                            <div class="col-md-9 normalText text-justify mb-4">
+                                <p>KEO Films, which had recently won a contract to make an Australian adaptation of a UK TV
+                                    series, was put in touch with Ecovis Sydney in November 2012 because they needed to
+                                    establish their imprint quickly, before Christmas!</p>
+                            </div>
+
+                        </div>
+                        <hr>
+                        <div class="d-flex">
+                            <div class="col-md-3 primaryHeading">Our Solutions</div>
+                            <div class="col-md-9 normalText text-justify mb-4">
+                                <p>We quickly identified the best structure for combining asset protection,
+                                    tax efficiency (in terms of their UK/Australia interaction), and
+                                    management of their new Australian business.</p>
+                                <p> This included a main trading company, a secondary company to hold a
+                                    primary production property, and a third company to hold the
+                                    intellectual property that they expected to develop in Australia.</p>
+                                <p> We had all three companies registered with ASIC (with David Conley as
+                                    resident director for each) and registered for all necessary taxes with
+                                    the ATO and OSR within a few days.</p>
+
+                                <p>We ensured that any necessary agreements between the three entities
+                                    and the UK parent were considered, in order to satisfy Australian/UK
+                                    laws and potential transfer pricing issues, by referring them to a local
+                                    commercial lawyer.</p>
+                                <p>We ensured that the client was advised on options for potential
+                                    secondments of UK employees to Australia to assist in the local
+                                    operations through a referral to a local immigration agent, and we
+                                    enabled the client to quickly determine the best fit for their needs
+                                    through a referral to several banks, and they were able to purchase.</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="d-flex">
+                            <div class="col-md-3 primaryHeading">Outcome</div>
+                            <div class="col-md-9 normalText text-justify mb-4">
+                                <p>KEO Films were benefitted for five years in Australian but due to
+                                    commercial reasons ceased operations in 2017 and reverted to a license
+                                    arrangement with local third-party producers.</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="d-flex mb-5">
+                            <div class="col-md-3 primaryHeading">Association</div>
+                            <div class="col-md-9 normalText text-justify mb-4">
+                                <p>David Conley, Director,
+
+                                    ECOVIS Clarc Jacobs, Sydney, Australia
+
+                                    david.conley@ecovis.com.au</p>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                    <div class="col-md-5">
+                        <p class="text-center mb-3"> Category: <span>Start-up</span>, <span>4th June, 2022</span></p>
+                        <div class=" text-center">
+                            <img src="./images/clientStoriesImages/baggingoutcomes_r.jpg" style="border-radius: 10px; filter:brightness(.5)" class="w-75" alt="">
+                        </div>
+                    <div class="">
+                <h4 class="text-center mainHeading mt-5 border-top pt-5">Similar stories</h4>
+                <p class="box_shadow px-4 mt-5 primaryHeading py-3 text-center" style="border-radius: 8px; white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;">Combined efforts bagging successful outcomes!</p>
+                <p class="box_shadow mt-5 px-4 primaryHeading py-3 text-center" style="border-radius: 8px; white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;">A Lending Hand for the procuration of the client!</p>
+                <p class="box_shadow mt-5 px-4 primaryHeading py-3 text-center" style="border-radius: 8px; white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;">Facilitation of collective outsources accounting, local tax compliance....</p>
+               
+            </div>
+                    </div>
+                </div>
+            </div>
+
+
         
-        <div class="row">
-            <div class="col-md-8">
-                <div>   
-                <h5>Rendering extensive start-up services to our clients!</h5>
-                <p>KEO Films, which had recently won a contract to make an Australian adaptation of a UK TV
-                series, was put in touch with Ecovis Sydney in November 2012 because they needed to
-                establish their imprint quickly, before Christmas!</p>
-                </div>
-                <div class="d-flex">
-                    <div class="col-md-3">The Context</div>
-                    <div class="col-md-9">The key reasons or questions for which the client associated with us and
-                      wanted us to completely focus were, How should Australia's presence
-                      be manifested? Could they acquire bank loan capital for a commercial
-                      property they had chosen for purchase while still being a start-up in
-                      Australia? How quickly could all of this occur?<br>
-                    </div>
-                    
-                </div><hr>
-                <div class="d-flex">
-                    <div class="col-md-3">Our Solutions</div>
-                    <div class="col-md-9">We quickly identified the best structure for combining asset protection,
-tax efficiency (in terms of their UK/Australia interaction), and
-management of their new Australian business.<br><br>
-
-
-
-This included a main trading company, a secondary company to hold a
-primary production property, and a third company to hold the
-intellectual property that they expected to develop in Australia.<br><br>
-
-
-
-We had all three companies registered with ASIC (with David Conley as
-resident director for each) and registered for all necessary taxes with
-the ATO and OSR within a few days.<br><br>
-
-
-
-We ensured that any necessary agreements between the three entities
-and the UK parent were considered, in order to satisfy Australian/UK
-laws and potential transfer pricing issues, by referring them to a local
-commercial lawyer.<br><br>
-
-
-We ensured that the client was advised on options for potential
-secondments of UK employees to Australia to assist in the local
-operations through a referral to a local immigration agent, and we
-enabled the client to quickly determine the best fit for their needs
-through a referral to several banks, and they were able to purchase.
-
-                    </div>
-                </div><hr>
-                <div class="d-flex">
-                    <div class="col-md-3">Outcome</div>
-                    <div class="col-md-9">KEO Films were benefitted for five years in Australian but due to
-commercial reasons ceased operations in 2017 and reverted to a license
-arrangement with local third-party producers.
-                    </div>
-                </div><hr>
-                <div class="d-flex mb-5">
-                    <div class="col-md-3">Association</div>
-                    <div class="col-md-9">David Conley, Director,
-
-ECOVIS Clarc Jacobs, Sydney, Australia
-
-david.conley@ecovis.com.au
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-md-4 border-left">
-                <h4 class="text-center">Similar stories</h4>
-                <p>Combined efforts bagging successful outcomes!</p>
-                <p>A Lending Hand for the procuration of the client!</p>
-                <p>Facilitation of collective outsources accounting, local tax compliance....</p>
-            </div>
         </div>
-        </div>
+    </div>
 
     <div class="container-fluid my-4">
         <div class="row">
             <div class="col-md-12 d-flex justify-content-center">
-                <button class="btn btn-secondary">PREVIOUS STORY</button>
-                <button class="btn btn-secondary mx-2">CLIENT STORIES</button>
-                <button class="btn btn-secondary">NEXT STORY</button>
+                <button class="btn box_shadow btn-dark normalText">PREVIOUS STORY</button>
+                <button class="btn box_shadow btn-danger normalText mx-2">CLIENT STORIES</button>
+                <button class="btn box_shadow btn-dark normalText">NEXT STORY</button>
             </div>
         </div>
     </div>
-    
+
     <!-- footer  -->
     <?php include_once 'footer.php'; ?>
 
